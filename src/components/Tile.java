117fe9d362b0961f46927a12dc92e98c8ca32183
connieksun <conniesun@email.arizona.edu>
2021-04-26 13:13:54 -0700

    oops, added a duplicate method

--- src/components/Tile.java
+++ src/components/Tile.java
@@ -25,7 +25,6 @@ public class Tile {
 	private boolean isCityTile = false;
 	private Unit unitHere = null;
 	private List<Player> revealedTo = new ArrayList<Player>();
-	private boolean isCity = false;
 
 	/**
 	 * When initially making a game, create every tile with a terrain type in mind.
@@ -66,7 +65,6 @@ public class Tile {
 			this.isCityTile = true;
 			this.movementBonus = 0; // TODO: figure out bonuses for units in cities
 			this.attackMult = 1; // subject to change
-			this.isCity = true;
 			return true;
 		}
 		return false;
@@ -172,10 +170,6 @@ public class Tile {
 	 */
 	public void revealTile(Player player) {
 		revealedTo.add(player);
-	}
-
-	public boolean isCityTile() {
-		return isCity;
 	}
 
 }
